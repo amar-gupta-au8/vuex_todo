fbd81fb7217d6a3e630915b328e13433e62ef945
theamargupta <amar@lestransformations.com>
2020-09-30 16:08:34 +0530

    intial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
+<script lang="ts">
+import { computed, defineComponent, onMounted } from 'vue';
+
+import NewItem from './components/NewItem.vue';
+import TodoList from './components/TodoList.vue';
+import { useStore } from './store/index';
+import { ActionTypes } from './store/actions';
+
+export default defineComponent({
+  components: { TodoList, NewItem },
+  setup() {
+    const store = useStore();
+
+    const loading = computed(() => store.state.loading);
+    onMounted(() => store.dispatch(ActionTypes.GetTodoItems));
+
+    const completedCount = computed(() => store.getters.completedCount);
+    const totalCount = computed(() => store.getters.totalCount);
+
+    return { loading, completedCount, totalCount };
+  },
+});
+</script>
+
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="container mx-auto mt-4">
+    <h1 class="text-3xl text-center p-2 font-bold">
+      Vue 3 Todo App with Typescript and Vuex 4
+    </h1>
+
+    <div v-if="loading">
+      <h3 class="text-center mt-4">Loading...</h3>
+    </div>
+    <div v-else>
+      <p class="text-center mt-2">
+        {{ completedCount }} of {{ totalCount }} completed.
+      </p>
+      <NewItem />
+      <TodoList />
+    </div>
   </div>
-  <router-view/>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
